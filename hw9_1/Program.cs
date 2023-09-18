44b3a6ffaee59fab7b3cdb1f6d0f29f7933faaa1
Yulia <meshokyulia0002@gmail.com>
2023-09-18 08:08:55 +0300

    hw91

--- hw9_1/Program.cs
+++ hw9_1/Program.cs
@@ -1,12 +1,14 @@
-﻿
-Console.Write("Введите значение M: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите значение N: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int sum(int m, int n)
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+PrintNumber(number);
+void PrintNumber(int n)
 {
-if (m==n)
-return n;
-else return m + sum(m + 1, n);
+    if (n == 1)
+        Console.Write(n + " ");
+    else
+    {
+        Console.Write(n + " ");
+        PrintNumber(n - 1);
+
+    }
 }
-Console.Write($"{sum(m,n)}");
