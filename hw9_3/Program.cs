4926c57aa302a5641ca2c72d4b69d4e881d6f97d
Yulia <meshokyulia0002@gmail.com>
2023-09-18 06:38:27 +0300

    hw93

---
+++ hw9_3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите чтсло N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int ak(int n, int m)
+{
+    if (n == 0)
+        return m + 1;
+    else
+      if ((n != 0) && (m == 0))
+        return ak(n - 1, 1);
+    else
+        return ak(n - 1, ak(n, m - 1));
+}
+
+Console.WriteLine($"Функция Аккермана равно: {ak(m, n)} ");
